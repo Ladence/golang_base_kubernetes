d772259b8855ef6781cc4669d4d1d7c38e7a6c2f
Ladence <zakhar.karlin11@gmail.com>
2022-01-15 18:01:22 +0300

    add routers test

---
+++ internal/server/router_test.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"encoding/json"
+	"github.com/Ladence/golang_base_kubernetes/internal/api"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouter(t *testing.T) {
+	r := NewRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/home")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code for /home is wrong. Have: %v, want: %v", res.StatusCode, http.StatusOK)
+	}
+
+	res, err = http.Post(ts.URL+"/home", "text/plain", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status code for POST /home is wrong. Have: %v, want: %v", res.StatusCode, http.StatusMethodNotAllowed)
+	}
+
+	res, err = http.Get(ts.URL + "/not-exist")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusNotFound {
+		t.Errorf("Status code for GET /not-exist is wrong. Have: %v, want: %v", res.StatusCode, http.StatusNotFound)
+	}
+}
+
+func TestHomeHandler(t *testing.T) {
+	w := httptest.NewRecorder()
+	home(w, nil)
+
+	resp := w.Result()
+	if have, want := resp.StatusCode, http.StatusOK; have != want {
+		t.Errorf("Status code is wrong. Have: %v, want: %v", have, want)
+	}
+
+	r := &api.HomeResponse{}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = json.Unmarshal(bytes, r)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
